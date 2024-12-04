59e13b71c60c0576886b3988587c0c848addf878
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-04 07:49:55 -0300

    29. State & Conditional Content

--- react-refresher/src/components/PostsList.jsx
+++ react-refresher/src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList() {
+	const [modalIsVisible, setModalIsVisible] = useState(true);
 	const [enteredBody, setEnteredBody] = useState("");
 	const [enteredAuthor, setEnteredAuthor] = useState("");
+
+	function hideModalHandler() {
+		setModalIsVisible(false);
+	}
 
 	function bodyChangeHandler(event) {
 		setEnteredBody(event.target.value);
@@ -18,12 +23,15 @@ function PostsList() {
 
 	return (
 		<>
-			<Modal>
-				<NewPost
-					onBodyChange={bodyChangeHandler}
-					onAuthorChange={authorChangeHandler}
-				/>
-			</Modal>
+			{modalIsVisible && (
+				<Modal onClose={hideModalHandler}>
+					<NewPost
+						onBodyChange={bodyChangeHandler}
+						onAuthorChange={authorChangeHandler}
+					/>
+				</Modal>
+			)}
+
 			<ul className={classes.posts}>
 				<Post author={enteredAuthor} body={enteredBody} />
 				<Post author="Manuel" body="Next.js" />
