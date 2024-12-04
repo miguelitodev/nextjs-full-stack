9aded3886a911ddd3c2176fc09f502205994dbb8
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-04 08:24:34 -0300

    32. Handling Form Submission

--- react-refresher/src/components/PostsList.jsx
+++ react-refresher/src/components/PostsList.jsx
@@ -1,36 +1,19 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-	const [enteredBody, setEnteredBody] = useState("");
-	const [enteredAuthor, setEnteredAuthor] = useState("");
-
-	function bodyChangeHandler(event) {
-		setEnteredBody(event.target.value);
-	}
-
-	function authorChangeHandler(event) {
-		setEnteredAuthor(event.target.value);
-	}
-
 	return (
 		<>
 			{isPosting && (
 				<Modal onClose={onStopPosting}>
-					<NewPost
-						onBodyChange={bodyChangeHandler}
-						onAuthorChange={authorChangeHandler}
-						onCancel={onStopPosting}
-					/>
+					<NewPost onCancel={onStopPosting} />
 				</Modal>
 			)}
 
 			<ul className={classes.posts}>
-				<Post author={enteredAuthor} body={enteredBody} />
 				<Post author="Manuel" body="Next.js" />
 			</ul>
 		</>
