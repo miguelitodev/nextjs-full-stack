66f76b55946cf5577b32da7f2dea8296a040c334
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-04 08:46:24 -0300

    34. Outputting List Data

--- react-refresher/src/components/PostsList.jsx
+++ react-refresher/src/components/PostsList.jsx
@@ -19,10 +19,20 @@ function PostsList({ isPosting, onStopPosting }) {
 					<NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
 				</Modal>
 			)}
+			{posts.length > 0 && (
+				<ul className={classes.posts}>
+					{posts.map(({ author, body }) => (
+						<Post key={body} author={author} body={body} />
+					))}
+				</ul>
+			)}
 
-			<ul className={classes.posts}>
-				<Post author="Manuel" body="Next.js" />
-			</ul>
+			{posts.length === 0 && (
+				<div style={{ textAlign: "center", color: "white" }}>
+					<h2>There are no posts yet.</h2>
+					<p>Start adding some!</p>
+				</div>
+			)}
 		</>
 	);
 }
