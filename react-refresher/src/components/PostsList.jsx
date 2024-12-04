4636168e2a3e8a3e1507f9d8cb2f694f1e6be2b2
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-04 07:23:45 -0300

    27. Lifting State Up

--- react-refresher/src/components/PostsList.jsx
+++ react-refresher/src/components/PostsList.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+	const [enteredBody, setEnteredBody] = useState("");
+	const [enteredAuthor, setEnteredAuthor] = useState("");
+
+	function bodyChangeHandler(event) {
+		setEnteredBody(event.target.value);
+	}
+
+	function authorChangeHandler(event) {
+		setEnteredAuthor(event.target.value);
+	}
+
 	return (
 		<>
-			<NewPost />
+			<NewPost
+				onBodyChange={bodyChangeHandler}
+				onAuthorChange={authorChangeHandler}
+			/>
 			<ul className={classes.posts}>
-				<Post author="Miguel" body="React.js" />
+				<Post author={enteredAuthor} body={enteredBody} />
 				<Post author="Manuel" body="Next.js" />
 			</ul>
 		</>
