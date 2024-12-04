be2c8e07bd2fa3d206759f09a45566e39e4c2ccc
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-03 21:15:26 -0300

    24. Preparing the App For State Management

--- react-refresher/src/components/PostsList.jsx
+++ react-refresher/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
 	return (
-		<ul className={classes.posts}>
-			<Post author="Miguel" body="React.js" />
-			<Post author="Manuel" body="Next.js" />
-		</ul>
+		<>
+			<NewPost />
+			<ul className={classes.posts}>
+				<Post author="Miguel" body="React.js" />
+				<Post author="Manuel" body="Next.js" />
+			</ul>
+		</>
 	);
 }
 
