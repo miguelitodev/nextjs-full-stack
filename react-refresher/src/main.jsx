941f0059bf3c0f34dc14dd5876c9a1e088fdcdc5
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-08 04:25:20 -0300

    41. Working with Layout Routes

--- react-refresher/src/main.jsx
+++ react-refresher/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NewPost from "./components/NewPost.jsx";
+import RootLayout from "./routes/RootLayout.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/",
+				element: <App />,
+			},
+			{ path: "/create-post", element: <NewPost /> },
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
