155f38714232595709212eba75ec4f943736f02a
Miguel Riquelme <miguelrisquelme@gmail.com>
2024-12-08 06:23:06 -0300

    91. Working with Pages & Layouts

--- nextlevel-food/app/layout.js
+++ nextlevel-food/app/layout.js
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+	title: "Nextleve Food",
+	description: "The place of your food",
+};
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="pt-BR">
